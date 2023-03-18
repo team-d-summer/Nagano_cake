9a4d196c23fecf83659dd7ac00c1d539f993d137
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-03-18 16:38:24 +0000

    orderdetail機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'order_details/update'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
