396bdf76757b3fa7231cb6884870321f63602f21
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-03-18 15:52:57 +0000

    a-customer機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
