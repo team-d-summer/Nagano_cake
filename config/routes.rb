f8a0a54ca8a5b7a55b767e54bc05144bdf479cf5
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-03-18 13:44:23 +0000

    Genre機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
