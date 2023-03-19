7d8b7f411d6a459dca400088a2324be47a04832b
Egyh <124222570+Egyh@users.noreply.github.com>
2023-03-19 09:32:40 +0000

    homesとitemsのコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ devise_for :customers,skip: [:passwords], controllers: {
 
  scope module: :public do
   root to: "homes#top"
-  resources :customers, only: [:show, :edit,:uodate,:unsubscribe,:withdrawl] 
+  resources :customers, only: [:show, :edit,:uodate,:unsubscribe,:withdrawl]
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-  
+
  end
 
 # 管理者用
@@ -22,6 +22,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
 namespace :admin do
   root to: "homes#top"
+  resources :items, except: [:destroy]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
