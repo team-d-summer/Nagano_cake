cc9c80d1b378309d41c359c8db5e4459bd389753
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-03-19 04:19:10 +0000

    Add　配送先

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :public do
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+  
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
