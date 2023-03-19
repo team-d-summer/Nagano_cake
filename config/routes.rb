9035d923ffc64285d0eda06fd2d5308c8a7b77a9
EC2 Default User <ec2-user@ip-172-31-30-185.ap-northeast-1.compute.internal>
2023-03-19 04:47:45 +0000

    log_in

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-
+ resources :customers, only: [:show, :edit,:uodate,:unsubscribe,:withdrawl] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
