c1d3ce7d31014074a835b9e1c8d7c7e870be6042
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-03-20 04:31:33 +0000

    routes.rb[修正]

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
 
 
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -15,17 +8,16 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 
+
+
  scope module: :public do
   root to: "homes#top"
   get "/homes/public/about" => "homes#about", as: "about"
   get 'customer' => 'customers#show'
   resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw] 
-  resources :items, only: [:index, :show]
-  delete "cart_items/destroy_all", to: "cart_items#destroy_all", as: "destroy_all"
-  resources :cart_items, only: [:index, :update, :destroy, :create]
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   resources :orders, only: [:index, :show, :create, :new]
-  
+  resources :items, only: [:index, :show]
  end
 
 
